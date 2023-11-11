1de7c0adc271c323de9d8c946d2dc236bd6d25bb
AlienX <aditya1115.ar@gmail.com>
2023-11-11 23:54:53 +0530

    post card added

---
+++ my-app/src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.once('connected', () => {
+      console.log('MongoDB connected successfully');
+    });
+
+    connection.on('error', () => {
+      console.log('MongoDB connection failed');
+      process.exit();
+    });
+  } catch (error) {
+    console.log('Something went wrong!');
+    console.log(error);
+  }
+}
