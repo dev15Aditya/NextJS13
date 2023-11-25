5bf784521e3edd3d71dab9ea089954ec8b3a0601
AlienX <aditya1115.ar@gmail.com>
2023-11-25 09:00:06 +0530

    implemented logout

--- my-app/src/app/api/users/login/route.ts
+++ my-app/src/app/api/users/login/route.ts
@@ -10,6 +10,7 @@ export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
     const { email, password } = reqBody;
+
     console.log(reqBody);
 
     //check if user exists
