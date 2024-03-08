90c4f61f2c6f41982d9d8b553aab43937f592fdd
AlienX <aditya1115.ar@gmail.com>
2024-03-08 16:11:32 +0530

    bg color fixed

--- chatbot/src/app/page.tsx
+++ chatbot/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   const callGetResponse = async () => {
     setIsLoading(true);
-    let temp = messages;
+    let temp = messages.slice(); // Create a copy of messages
 
     temp.push({ role: 'user', content: theInput });
     setMessages(temp);
@@ -34,8 +34,6 @@ export default function Home() {
 
     const { output } = data;
 
-    //console.log('Got a response...', output.content);
-
     setMessages((prev) => [...prev, output]);
 
     setIsLoading(false);
@@ -48,52 +46,48 @@ export default function Home() {
     }
   };
 
-  // new chat should hide old chat should scroll up to show new chat
   useEffect(() => {
-    const chat = document.querySelector('.h-full');
+    const chat = document.querySelector('.overflow-y-auto');
     chat?.scrollTo(0, chat.scrollHeight);
-  }, [messages]);
+  });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-5">
-      <h1 className="text-5xl font-sans">ChatBot🤖</h1>
+    <main className="flex flex-col items-center justify-between md:px-24 py-4 md:py-5 h-screen overflow-y-hidden">
+      <h1 className="text-3xl md:text-5xl font-sans mb-4">ChatBot🤖</h1>
 
-      <div className="flex  h-[25rem] w-[35rem] flex-col items-center bg-gray-600 rounded-sm">
-        <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
-          {messages.map((e) => {
-            return (
-              <div
-                key={e.content}
-                className={`w-max max-w-[18rem] rounded-md px-4 py-3 h-min ${
-                  e.role === 'assistant'
-                    ? 'self-start  bg-gray-200 text-gray-800'
-                    : 'self-end  bg-gray-800 text-gray-50'
-                } `}
-              >
-                {e.content}
-              </div>
-            );
-          })}
+      <div className="flex flex-col items-center justify-between bg-gradient-to-r from-slate-900 to-slate-700 rounded-sm w-full md:w-[35rem] h-[90%] md:h-[85%]">
+        <div className="flex flex-col gap-2 overflow-y-auto py-4 px-2 md:py-8 md:px-3 my-2 w-full h-[85%]">
+          {messages.map((e, index) => (
+            <div
+              key={index}
+              className={`w-max max-w-[18rem] rounded-md px-4 py-3 ${
+                e.role === 'assistant'
+                  ? 'self-start bg-gray-800 text-gray-50'
+                  : 'self-end bg-blue-500 text-gray-50'
+              }`}
+            >
+              {e.content}
+            </div>
+          ))}
 
-          {isLoading ? (
-            <div className="self-start  bg-gray-200 text-gray-800 w-max max-w-[18rem] rounded-md px-4 py-3 h-min">
+          {isLoading && (
+            <div className="self-start bg-gray-200 text-gray-800 w-max max-w-[18rem] rounded-md px-4 py-3">
               *thinking*
             </div>
-          ) : (
-            ''
           )}
         </div>
-        <div className="relative  w-[80%] bottom-4 flex justify-center">
+
+        <div className="relative w-[90%] bottom-3 flex justify-center">
           <textarea
             value={theInput}
             onChange={(event) => setTheInput(event.target.value)}
-            className="w-[85%] h-10 px-3 py-2
-            resize-none overflow-y-auto text-black bg-gray-300 rounded-l outline-none"
+            className="w-[70%] md:w-[75%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rounded-l outline-none"
             onKeyDown={Submit}
+            placeholder="Type to start the conversation..."
           />
           <button
             onClick={callGetResponse}
-            className="w-[15%] bg-blue-500 px-4 py-2 rounded-r"
+            className="w-[30%] md:w-[25%] bg-blue-500 px-4 py-2 rounded-r"
           >
             send
           </button>
