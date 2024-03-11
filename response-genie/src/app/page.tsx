80603d255f63be7e1c2f4f38b0bdcff1df23c38e
AlienX <aditya1115.ar@gmail.com>
2024-03-11 22:41:42 +0530

    working on SignInpage

--- response-genie/src/app/page.tsx
+++ response-genie/src/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
+import { useSession } from 'next-auth/react';
+import SigninButton from './components/SigninButton';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between"></main>
+    <main className="flex h-screen flex-col items-center justify-between">
+      <h1 className="text-4xl font-bold">Welcome to MailGienie</h1>
+      <p className="text-lg">
+        MailGienie is a simple email client to manage your emails.
+      </p>
+
+      <SigninButton />
+    </main>
   );
 }
--- response-genie/src/app/pages/login/page.tsx
+++ response-genie/src/app/pages/login/page.tsx
@@ -1,4 +1,20 @@
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export default function Login() {
+  const { data: session } = useSession();
+
+  if (session && session.user) {
+    return (
+      <div className="flex gap-4 ml-auto">
+        <p className="text-sky-600">{session.user.name}</p>
+        <button onClick={() => signOut()} className="text-red-600">
+          Sign Out
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="max-h-fit flex justify-center text-white my-12">
       <div className="max-w-md w-full p-8 bg-gray-800 rounded-lg shadow-lg border border-gray-500 transition-colors hover:border-gray-300">
@@ -40,6 +56,10 @@ export default function Login() {
           </a>
         </p>
       </div>
+
+      <button onClick={() => signIn()} className="text-green-600 ml-auto">
+        Sign In
+      </button>
     </div>
   );
 }
